3d3deff8783980d6f7b064152239804fb93fc17d
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2020-03-31 19:29:55 -0300

    Modified install.sh for linux

--- install.sh
+++ install.sh
@@ -6,25 +6,15 @@ set -e
 echo "Initializing dotfiles for a $current_os setup..."
 echo "";
 
-if [ $current_os="Linux" ]; then
-    echo "Update..."
-    apt-get update
-fi
 
 if [ ! -d "$HOME/dotfiles" ]; then
     echo "Cloning dotfiles to $HOME/dotfiles"
-    #git clone https://https://github.com/cesmunoz/dotfiles.git $HOME/dotfiles
+    git clone git@github.com:cesmunoz/dotfiles.git $HOME/dotfiles
 else
     echo "$HOME/dotfiles found - Skipping clone repository"
 fi
 
-echo "";
-echo "Checking ZSH..."
-if [ ! -f "$HOME/.zshrc" && $current_os="Linux" ]; then
-    apt-get install zsh -y
-fi
-
-#cd "$HOME/dotfiles"
+cd "$HOME/dotfiles"
 
 dotfiles=(".zshrc" ".bashrc" ".vimrc")
 if [ $current_os == "MINGW64" ]; then
@@ -33,7 +23,7 @@ fi
 
 echo "";
 echo "Symlink default dotfiles with backups..."
-for file in ${dotfiles[@]} do
+for file in ${dotfiles[@]}; do
     if [ -h $HOME/$file ]; then
         echo "Skipping $HOME/$file since it is already a symlink..." 
     else
@@ -49,19 +39,44 @@ for file in ${dotfiles[@]} do
     fi  
 done
 
-echo "";
-echo "Checking oh-my-zsh..."
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-  echo "Installing oh-my-zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended  
-fi
-
 echo ""
-if [[ ! -d "$HOME/.nvm" ]]; then 
-  echo "Installing nvm..."
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+echo "Adding $current_os specific functionality..."
+if [ $current_os="Linux" ]; then
+    echo "Update Libraries..."
+    apt-get update
 
-  source "$HOME/.zshrc"
-else
-  echo "nvm installation found. Skipping..."
+    echo "";
+    echo "Checking ZSH..."
+    if [ $current_os="Linux" ]; then
+        echo "Install ZSH..."
+        apt-get install zsh -y
+    fi
+    
+    echo "";
+    echo "Checking oh-my-zsh..."
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo "Installing oh-my-zsh"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended  
+    fi
+
+    echo ""
+    if [ ! -d "$HOME/.nvm" ]; then 
+        echo "Installing nvm..."
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+  
+        source .bashrc
+    else
+        echo "nvm installation found. Skipping..."
+    fi
+
+    if [ ! -x "$(command -v node)" ]; then
+        echo "Installing node..."
+        nvm install stable
+        nvm alias default stable
+        nvm use default
+
+        source $HOME/.bashrc
+    else
+        echo "node found... skipping installation"
+    fi
 fi
