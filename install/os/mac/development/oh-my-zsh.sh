79e1c59ceb3224e9e98241db9381ba7da8cec049
Cesar Muñoz <cm@Cesars-MacBook-Pro.local>
2025-08-17 01:04:49 +0200

    fix: check oh my zsh preinstallation and change mise to activate to zsh

--- install/os/mac/development/oh-my-zsh.sh
+++ install/os/mac/development/oh-my-zsh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Check if Oh My Zsh is installed
+if [ -d "$HOME/.oh-my-zsh" ]; then
+	echo "Oh My Zsh is already installed."
+else
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
