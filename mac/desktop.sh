7a4fb01b77f1e8907eba3ddebddfe6a01c129e4c
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2025-08-16 14:05:53 +0200

    fix: remove aerospace and alacritty - changes to aliases and desktop

--- mac/desktop.sh
+++ mac/desktop.sh
@@ -14,8 +14,6 @@ brew install --cask\
   postman \
   spotify \
   discord \
-  alacritty \
-  sf-symbols
 
 
 OPTIONAL_APPS=("slack" "studio-3t" "loom" "mongodb-compass")
@@ -26,26 +24,6 @@ if [ -n "$OPTIONAL_APPS" ]; then
     brew install --cask $tool
   done
 fi
-
-# Aerospace configuration
-brew install --cask nikitabobko/tap/aerospace
-
-## Junkyborders
-brew tap FelixKratz/formulae
-brew install borders
-
-## Sketchybar
-brew tap FelixKratz/formulae
-brew install sketchybar
-
-mkdir -p ~/.config/sketchybar/plugins
-cp $(brew --prefix)/share/sketchybar/examples/sketchybarrc ~/.config/sketchybar/sketchybarrc
-cp -r $(brew --prefix)/share/sketchybar/examples/plugins/ ~/.config/sketchybar/plugins/
-
-brew install --cask font-hack-nerd-font
-
-brew services start sketchybar
-brew services start borders
 
 # Warp
 defaults write dev.warp.Warp-Stable AltScreenPaddingOverride true
