9ba2606bbd18851d57c57c190e2633759ecda7ff
Cesar Muñoz <cm@Cesars-MacBook-Pro.local>
2025-08-17 01:14:47 +0200

    feat: adding ghostty

--- install/os/mac/development/stow.sh
+++ install/os/mac/development/stow.sh
@@ -2,5 +2,6 @@
 
 stow --adopt --target=$HOME --dir="$REPO_DIR/dotfiles" zsh
 stow --adopt --target=$HOME --dir="$REPO_DIR/dotfiles" git
+stow --adopt --target=$HOME --dir="$REPO_DIR/dotfiles" ghostty
 
 # need to stow the specific from the os
