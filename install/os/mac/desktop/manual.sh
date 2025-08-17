f78d5559cd49de91e2eec120a436934450160b2e
Cesar Muñoz <cm@Cesars-MacBook-Pro.local>
2025-08-17 16:41:07 +0200

    feat: adding raycast configuration

--- install/os/mac/desktop/manual.sh
+++ install/os/mac/desktop/manual.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
 gum style \
-	--foreground 212 --border-foreground 212 --border double \
-	--align center --width 50 --margin "0 0" --padding "1 4" \
-	'Manual Installation'
+    --foreground 212 --border-foreground 212 --border double \
+    --align center --width 50 --margin "0 0" --padding "1 4" \
+    'Manual Installation'
 
-gum confirm "Install MongoDb Compass"
-gum confirm "Install Autofirma Compass"
+gum confirm "Install MongoDB Compass"
+gum confirm "Install Autofirma"
+
+gum style \
+    --foreground 212 --border-foreground 212 --border double \
+    --align left --width 50 --margin "0 0" --padding "1 4" \
+    'Raycast Configuration' \
+    '' \
+    '• Set up browser shortcuts' \
+    '• Set up DB shortcut' \
+    '• Set up Notes shortcut' \
+    '• Set up Reminders shortcut' \
+    '• Set up Safari shortcut' \
+    '• Set up Slack shortcut' \
+    '• Set up Spotify shortcut' \
+    '• Set up VSCode shortcut' \
+    '• Set up Terminal shortcut' \
+    '• Enable Switch Tabs (Alt+Tab)' \
+    '• Configure Window Management'
+
+gum confirm "Did you finish Raycast configuration?"
