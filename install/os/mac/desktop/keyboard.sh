4e23c6a36d9ff7944dd4e2d2f44f15b6db1a4b0f
Cesar Muñoz <cm@Cesars-MacBook-Pro.local>
2025-08-17 16:34:25 +0200

    feat: adding fn configuration

--- install/os/mac/desktop/keyboard.sh
+++ install/os/mac/desktop/keyboard.sh
@@ -2,3 +2,6 @@
 
 # Remap Caps Lock (bloq mayus) to Escape on macOS
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
+
+# i want to have F1-F12 keys without pressing Fn key
+defaults write -g com.apple.keyboard.fnState -bool true
