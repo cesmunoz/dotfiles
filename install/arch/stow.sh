c98ff5bfebb37dfcda2ac2dd1c2baa41c3865428
cesar munoz <cesar.alberto.munoz@gmail.com>
2025-08-22 18:31:49 +0200

    test: another test for stow

--- install/arch/stow.sh
+++ install/arch/stow.sh
@@ -3,9 +3,11 @@
 cp $DOTFILES_DIR/zsh/.zshrc $HOME/.zshrc
 cp $DOTFILES_DIR/hypr/.config $HOME/.config/hypr/hyprland.conf
 
-stow -R -v -t $HOME -d $DOTFILES_DIR zsh
+stow --adopt -v -t $HOME -d $DOTFILES_DIR zsh
+
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR git
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR ghostty
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR starship
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR nvim
-stow -R -v -t $HOME -d $DOTFILES_DIR hypr
+
+stow --adopt -R -v -t $HOME -d $DOTFILES_DIR hypr
