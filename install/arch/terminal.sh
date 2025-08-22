67841321a95732b6bbc47b9f6e7a0a16404dcc0f
cesar munoz <cesar.alberto.munoz@gmail.com>
2025-08-22 15:04:40 +0200

    fix: right path to remove init.lua

--- install/arch/terminal.sh
+++ install/arch/terminal.sh
@@ -48,7 +48,7 @@ fi
 # Kickstart Neovim
 rm -rf ~/.config/nvim
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-rm ~/config/nvim/init.lua
+rm ~/.config/nvim/init.lua
 
 # AWS cli
 if ! command -v aws &> /dev/null; then
