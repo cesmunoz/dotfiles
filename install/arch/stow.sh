ac00dec6b857093cbee15fa7a3991fc0492d340a
cesar munoz <cesar.alberto.munoz@gmail.com>
2025-08-22 18:31:05 +0200

    test: copying hyperland

--- install/arch/stow.sh
+++ install/arch/stow.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -f ~/.zshrc
-rm -f ~/.config/hypr/hyprland.conf
+cp $DOTFILES_DIR/zsh/.zshrc $HOME/.zshrc
+cp $DOTFILES_DIR/hypr/.config $HOME/.config/hypr/hyprland.conf
 
-stow --adopt --target=$HOME --dir=$DOTFILES_DIR zsh
+stow -R -v -t $HOME -d $DOTFILES_DIR zsh
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR git
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR ghostty
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR starship
 stow --adopt --target=$HOME --dir=$DOTFILES_DIR nvim
-stow --adopt --target=$HOME --dir=$DOTFILES_DIR hypr
+stow -R -v -t $HOME -d $DOTFILES_DIR hypr
